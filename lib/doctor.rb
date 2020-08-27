13b2da7cb3fb18ada2d47c950232ca6ae5c39938
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-27 23:51:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,7 +13,7 @@ class Doctor
   end
   
   def appointments
-    Appointments.all.select{|appt| appt.doctor == self}
+    Appointment.all.select{|appt| appt.doctor == self}
   end
   
   def new_appointment(date, patient) 
